97a1b58c20c74d4eaf465719303773a0814734fc
catclou <caterina.moccia@gmail.com>
2022-03-16 11:54:36 +0100

    polished 1st snack

--- snack 1/assets/JS/myscript.js
+++ snack 1/assets/JS/myscript.js
@@ -1,16 +1,13 @@
+// 1) Chiedi all'utente di inserire due numeri
 let primoNumero = prompt("Digita il primo numero (in cifre)");
-
 let secondoNumero = prompt("Digita il secondo numero (in cifre)");
 
-console.log(primoNumero, secondoNumero);
-
+// 2) Metti i due numeri a confronto
+// 3) Stampa quello che risulta maggiore
 if (primoNumero > secondoNumero) {
-    console.log(`Il primo numero è maggiore`);
     document.getElementById("risultato").innerHTML = `Il primo numero (${primoNumero}) è maggiore`;
 } else if (primoNumero < secondoNumero) {
-    console.log(`Il secondo numero è maggiore`);
     document.getElementById("risultato").innerHTML = `Il secondo numero (${secondoNumero}) è maggiore`;
 } else {
-    console.log(`Hai scritto due numeri uguali!`);
     document.getElementById("risultato").innerHTML = `Hai inserito due numeri uguali...`;
 }
